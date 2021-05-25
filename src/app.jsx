17aa8dc749247fea7730b07e515cdbfe48a56563
Nolan Docherty <sodinokazaki@gmail.com>
2021-05-25 22:27:13 +0100

    add babel and react.

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function render() {
+    ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+}
+
+render();
--- src/renderer.js
+++ src/renderer.js
@@ -27,5 +27,5 @@
  */
 
 import './index.css';
-
+import './app.jsx'
 console.log('👋 This message is being logged by "renderer.js", included via webpack');
