e194ae4d54e8e97f1858fbecd9c1111ddc54001d
Nolan Docherty <sodinokazaki@gmail.com>
2021-06-01 22:13:06 +0100

    add a style to the html

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,8 +3,12 @@ import {CpuTemperature} from "./cpu-temprature/cpu-temperature.jsx";
 import {WeatherTemperature} from "./weather-temperature/weather-temperature.jsx";
 
 export function Main() {
-    return <div>
-        <CpuTemperature/>
-        <WeatherTemperature/>
+    return <div className='main-contents'>
+        <div className='cpu-temperature'>
+            <CpuTemperature/>
+        </div>
+        <div className='weather-temperature'>
+            <WeatherTemperature/>
+        </div>
     </div>
 }
