188869f77fd6355f31389ab0d9433e9117e9a1e2
Nolan Docherty <sodinokazaki@gmail.com>
2021-06-01 22:41:48 +0100

    add comments for weatherBit api

--- src/services/weather.js
+++ src/services/weather.js
@@ -3,11 +3,13 @@ import { weatherBitUrl, weatherBitKey} from "../shared/constants";
 export async function getAmbientTemperature() {
 
     const {data} = await weatherApiCall();
+    // full list of attributes for the response in constants.js or
     const {[0]: {temp}} = data;
     return temp;
 }
 
 function weatherApiCall() {
+    // weatherbit API docs https://www.weatherbit.io/api/weather-current
 
     const requestString = `?key=${weatherBitKey}&lang=en&units=M&lat=53.944&lon=-1.088`;
 
